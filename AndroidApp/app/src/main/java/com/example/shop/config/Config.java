132cb9c75f55c419833233996df3d7c20047d66a
novakvova <novakvova@gmail.com>
2024-06-20 21:04:32 +0300

    Test Android Host

--- AndroidApp/app/src/main/java/com/example/shop/config/Config.java
+++ AndroidApp/app/src/main/java/com/example/shop/config/Config.java
@@ -1,4 +1,5 @@
 package com.example.shop.config;
 public class Config {
-    public static final String BASE_URL = "http://192.168.50.18:5174/";
+//    public static final String BASE_URL = "http://192.168.50.18:5174/";
+    public static final String BASE_URL = "https://android.novakv.com/";
 }
